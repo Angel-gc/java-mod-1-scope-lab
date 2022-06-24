dc820143112e804a0b2a54749ed16e64aa897e1d
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-24 11:28:41 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        int num;
+        boolean flag = false;
+        if (flag) {
+            // do something when flag is true
+            num = 12;
+        } else {
+            // do something else when flag is false
+            num = 5;
+        }
+        System.out.println(num);
     }
 }
